b96910f195c33fdd33a91dc33a11778b238f065c
Your Name <11@111.com>
2019-06-18 21:06:02 +0800

    618商品完善

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,50 @@
 import ajax from "./ajax";
-import Password from "antd/lib/input/Password";
+import {message} from "antd";
+import jsonp from 'jsonp'
 const BASE = ''
-export const reqLogin = (username,password) => ajax(BASE+'/login',{username,password},'POST')
-export const reqAddUser = (user) => ajax(BASE +'/manage/user/add',user,'POST')
-reqLogin('admin','admin').then(result =>{
-  console.log('result',result)
-})
+
+export const reqLogin = (username, password) => ajax(BASE + '/login', {username, password}, 'POST')
+
+// 添加用户
+export const reqAddUser = (user) => ajax(BASE + '/manage/user/add', user, 'POST')
+
+
+// 获取分类列表(一级/二级)
+export const reqCategorys = (parentId) => ajax(BASE + '/manage/category/list', {parentId})
+
+// 更新分类的名称
+export const reqUpdateCategory = ({categoryId, categoryName}) => ajax(BASE + '/manage/category/update', {categoryId, categoryName}, 'POST')
+
+// reqLogin('admin','admin').then(result =>{
+//   console.log('result',result)
+// })
+
+/* 
+发jsonp请求获取当前天气信息
+*/
+export const reqWeather = (location) => {
+  const url = `http://api.map.baidu.com/telematics/v3/weather?location=${location}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+
+  return new Promise((resolve, reject) => {
+    
+    // 执行请求
+    setTimeout(() => {
+      jsonp(url, {}, (error, data) => {
+        if (!error && data.status === 'success') {
+          const {
+            dayPictureUrl,
+            weather
+          } = data.results[0].weather_data[0]
+          // 成功了, 调用reolve()指定成功的值
+          resolve({
+            dayPictureUrl,
+            weather
+          })
+        } else {
+          message.error('获取天气信息失败!')
+        }
+      })
+    }, 2000)
+  })
+  
+}
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,130 @@
+//Admin 头部界面
 import React, { Component } from 'react'
+import {withRouter} from 'react-router-dom'
+import { Modal } from 'antd'
 
-export default class Admin extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+import LinkButton from "../../components/link-button"
+import {reqWeather} from '../../api'
+import {removeUser} from "../../utils/storageUtils"
+import memoryUtils from "../../utils/memoryUtils"
+import {formateDate} from '../../utils/dateUtils'
+import menuList from '../../config/menuConfig'
+import './index.less'
+
+
+ class Header extends Component {
+  
+  //初始化状态
+  state = {
+      //给Header的实例（组件对象）添加属性
+      currentTime:formateDate(Date.now()),//  当前时间字符串
+      dayPictureUrl:'', //天气图片url
+      weather:'',//天气的文本
+  }
+  //每隔1s更新时间的显示
+  showCurrentTime =()=>{
+      this.itemrvalId = setInterval(() =>{
+          const currentTime = formateDate(Date.now())
+          //更新状态
+          this.setState({
+              currentTime
+          })
+      },1000);
+  }
+
+  //得到当前请求路径对应的title
+  getTitle =()=> {
+      //得到当前请求路径
+      const path = this.props.location.pathname
+      let title =''
+      menuList.forEach(item => {
+          if(item.key === path){
+              title = item.title
+          }else if(item.children){
+              const cItem = item.children.find(item => item.key === path)
+              if(cItem){
+                 title = cItem.title
+              }
+          }
+      })
+      return title
+  }
+
+  //获取天气信息显示
+  getWeather = async () => {
+      const {dayPictureUrl, weather} = await reqWeather('北京')
+       // 更新状态
+    this.setState({
+        dayPictureUrl, 
+        weather 
+     })
+  }
+
+    
+  /* 
+  退出登陆
+  */
+  logout = () => {
+
+    // 显示确认框, 点击确定后再退出
+    Modal.confirm({
+      title: '确定退出吗?',
+      onOk: () => {
+        console.log('OK')
+        // 清除保存user数据
+        removeUser()  // local中的
+        memoryUtils.user = {} // 内存中的
+        // 跳转到登陆
+        this.props.history.replace('/login')
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+    })
+  }
+
+  /* 
+  组件即将销毁之前
+  做一些收尾的工作: 清除定时器
+  */
+  componentWillUnmount () {
+    clearInterval(this.itervalId)
+  }
+
+  componentDidMount () {
+    // 每隔1s更新时间的显示
+    this.showCurrentTime()
+    // 获取天气信息显示
+    this.getWeather()
+  }
+
+  render() {
+    const { currentTime, dayPictureUrl, weather } = this.state
+
+    // 得到当前登陆的用户
+    const { user } = memoryUtils
+    // 得到当前请求路径对应的title
+    const title = this.getTitle()
+
+
+
+    return (
+      <div className="header">
+        <div className="header-top">
+          <span>欢迎, {user.username}</span>
+         
+          <LinkButton onClick={this.logout}>退出</LinkButton>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">{title}</div>
+          <div className="header-bottom-right">
+            <span>{currentTime}</span>
+            {!!dayPictureUrl && <img src={dayPictureUrl} alt="weather" />}
+            <span>{weather}</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+export default withRouter(Header)
